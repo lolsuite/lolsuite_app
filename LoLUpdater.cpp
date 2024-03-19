4d345ea865501e7ed9cc51c89e29210372608c6b
lolsuite <mobasuite@outlook.com>
2024-03-19 18:06:42 +0100

    formatting problems

--- LoLUpdater.cpp
+++ LoLUpdater.cpp
@@ -1090,10 +1090,7 @@ void trackmania(bool restore)
 	exit(0);
 }
 
-int APIENTRY WinMain(_In_ HINSTANCE hInstance,
-	_In_opt_ HINSTANCE hPrevInstance,
-	_In_ LPWSTR lpCmdLine,
-	_In_ int nCmdShow)
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
